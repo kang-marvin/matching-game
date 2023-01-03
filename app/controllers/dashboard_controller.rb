781ae11cb1385768ded8484714f37479c339ccd4
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-01-03 19:53:31 +0300

    Update the main Game dashboard using the create view components for Header, Informants and Board

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class DashboardController < ApplicationController
 
-  def index; end
+  def index
+    @game_board_size = 3
+
+    @game_board = Array.new(@game_board_size) do
+      Array.new(@game_board_size) { rand(5) }
+    end
+  end
 
 end
