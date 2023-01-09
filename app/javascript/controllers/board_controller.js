f0421010795c5def4347fd657da1da635052eddc
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-01-09 21:31:24 +0300

    Update background color and label of the checked tile

--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -3,11 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 const defaultColor      = 'bg-black'
 const timeoutInterval   = 1000
 
+JOINER = "--"
+DEFAULT_RESULT = { color: "white", label: "N/A" }
+
 // Connects to data-controller="board"
 export default class extends Controller {
 
-  static values  = { board: Object }
-  static targets = [ "test" ]
+  static values  = { finishedResult: {type: Object, default: {} } }
 
   initialize() {
     console.log("Board controller initialized")
@@ -15,23 +17,38 @@ export default class extends Controller {
 
   connect() {
     console.log("Board controller connected")
+    this.boardKeys = Object.keys(this.finishedResultValue)
     this.totalTileClicked = 0
   }
 
   flipTile(event) {
     const tile = event.target
-    this.showTileContents(tile)
+    const data =
+      this.fetchDataForClickedTile(tile.dataset.tileIndex)
+
+    this.showTileContents(tile, data)
     setTimeout(() => {
       this.resetTileContents(tile)
     }, timeoutInterval)
 
     this.totalTileClicked++
-    this.testTarget.textContent = this.totalTileClicked
+    // this.testTarget.textContent = this.totalTileClicked
   }
 
-  showTileContents(tile) {
-    tile.style.backgroundColor = "yellow"
-    tile.textContent = "X"
+  fetchDataForClickedTile(tileIndex) {
+    const tileKeyInBoard = this.boardKeys.filter(key => {
+      return this.finishedResultValue[key].includes(Number(tileIndex))
+    })
+
+    if (tileKeyInBoard.length < 1) { return DEFAULT_RESULT }
+    const result = tileKeyInBoard[0].split(JOINER)
+
+    return { color: result[0], label: result[1] }
+  }
+
+  showTileContents(tile, data) {
+    tile.style.backgroundColor = data.color
+    tile.textContent = data.label
     tile.classList.remove(defaultColor)
   }
 
