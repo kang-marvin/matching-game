84f529a045328557c2977468baf735d6ed92d8cd
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-01-09 21:30:48 +0300

    Rebuild the board generator

--- app/components/game_board_component.rb
+++ app/components/game_board_component.rb
@@ -2,15 +2,32 @@
 
 class GameBoardComponent < ViewComponent::Base
 
-  SUPPORTED_BOARD_SIZE = [ 2, 4 ].freeze
+  SUPPORTED_BOARD_SIZE = [ 2, 4, 6 ].freeze
+  JOINER = "--"
 
-  def initialize(size: SUPPORTED_BOARD_SIZE[0])
+  def initialize(size: SUPPORTED_BOARD_SIZE[0], board: [])
     @size = size
     @board_size = (size * size)
-    @decoded_object = { "yellow--rock": [ 2, 13 ] }.to_json
+    @board_object = hashfied_board(board).to_json
   end
 
   def render?
     SUPPORTED_BOARD_SIZE.include?(@size)
+  end
+
+  private
+
+  def hashfied_board(board)
+    result = {}
+    board
+      .group_by.with_index { |_, index| index }
+      .transform_values { |value| value.join(JOINER)}
+      .each do |key, value|
+        result[value] ||= []
+        result[value] << key
+        result[value].uniq!
+      end
+
+    result
   end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,24 @@
 class DashboardController < ApplicationController
 
+  COLORS = ['red', 'green', 'blue', 'pink', 'orange', 'brown']
+  LETTERS = ['Dog', 'Cats', 'Monkey', 'Ostrich', 'Pig', 'Fish']
+
   def index
     @game_board_size = 4
+    @game_board = board_generator(@game_board_size)
+  end
 
-    @game_board = Array.new(@game_board_size) do
-      Array.new(@game_board_size) { rand(5) }
-    end
+  private
+
+  def board_generator(board_size)
+    color_options = COLORS.shuffle.take(board_size)
+    letter_options = LETTERS.shuffle.take(board_size)
+    options = color_options.product(letter_options)
+
+    uniq_options =
+      options.rotate(rand(10)).each_slice(options.count / 2).to_a.sample
+
+    (uniq_options * 2).shuffle
   end
 
 end
