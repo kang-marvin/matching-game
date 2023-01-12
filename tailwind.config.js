703a378edbbf8d3629fdf5daa3c0974c8fc91d88
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-01-12 13:11:39 +0300

    Purge the classes that are updated dynamically

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,12 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  purge: {
+    safelist: [
+      'grid-cols-2', 'grid-rows-2',
+      'grid-cols-4', 'grid-rows-4',
+      'grid-cols-6', 'grid-rows-6',
+    ],
+  },
 }
