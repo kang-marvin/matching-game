aa30f3d6241d799efaf9575044cd1671edd7c2fe
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-01-16 11:57:21 +0300

    Add select tag for game levels and default value set

--- app/components/game_informant_component.rb
+++ app/components/game_informant_component.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class GameInformantComponent < ViewComponent::Base
+  GAME_DIFFICULTY_LEVELS = [ 'Easy', 'Standard', 'Hard' ]
+
+  def initialize()
+    @game_levels = GAME_DIFFICULTY_LEVELS
+    @default_game_level = 'Hard'
+  end
 end
