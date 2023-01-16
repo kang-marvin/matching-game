f66b311f306085446e2aa0e6cc213daff202391b
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-01-16 14:42:23 +0300

    Change the root path to have '/game'

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "dashboard#index"
+
+  root to: redirect(path: '/game')
+  get '/game', to: "dashboard#index"
+
 end
